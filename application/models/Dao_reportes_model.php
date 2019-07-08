731037f7d768e1083d30f33b3a9b1c8d23717a67
JhonChaparro <jfchaparro33@misena.edu.co>
2019-07-08 09:00:26 -0500

    correcion query slas movil se corrigue el query de los slas de la movil para que si el tiempo de escalamiento se encuentra en 0.000 tome el tiempo de vida del incidente

--- application/models/Dao_reportes_model.php
+++ application/models/Dao_reportes_model.php
@@ -45,14 +45,14 @@ class Dao_reportes_model extends CI_Model {
                     WHEN DESCRIPTION LIKE '%FOIP%' THEN 'FOIP'
                     ELSE 'Sin coordinación'
                 END) AS coordinacion,
-                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 1 AND TIEMPO_ESCALA <= 20, 1, 0)) AS 'alta_alta_20_min',
-                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 1 AND TIEMPO_ESCALA > 20, 1, 0)) AS 'alta_alta_20_max',
-                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 2 AND TIEMPO_ESCALA <= 40, 1, 0)) AS 'alta_media_40_min',
-                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 2 AND TIEMPO_ESCALA > 40, 1, 0)) AS 'alta_media_40_max',
-                SUM(IF(INTERNALPRIORITY = 2 AND TIEMPO_ESCALA <= 60, 1, 0)) AS 'medias_60_min',
-                SUM(IF(INTERNALPRIORITY = 2 AND TIEMPO_ESCALA > 60, 1, 0)) AS 'medias_60_max',
-                SUM(IF(INTERNALPRIORITY = 3 AND TIEMPO_ESCALA <= 80, 1, 0)) AS 'bajas_80_min',
-                SUM(IF(INTERNALPRIORITY = 3 AND TIEMPO_ESCALA > 80, 1, 0)) AS 'bajas_80_max',
+                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 1 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) <= 20, 1, 0)) AS 'alta_alta_20_min',
+                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 1 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) > 20, 1, 0)) AS 'alta_alta_20_max',
+                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 2 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) <= 40, 1, 0)) AS 'alta_media_40_min',
+                SUM(IF(INTERNALPRIORITY = 1 AND URGENCY = 2 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) > 40, 1, 0)) AS 'alta_media_40_max',
+                SUM(IF(INTERNALPRIORITY = 2 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) <= 60, 1, 0)) AS 'medias_60_min',
+                SUM(IF(INTERNALPRIORITY = 2 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) > 60, 1, 0)) AS 'medias_60_max',
+                SUM(IF(INTERNALPRIORITY = 3 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) <= 80, 1, 0)) AS 'bajas_80_min',
+                SUM(IF(INTERNALPRIORITY = 3 AND IF(TIEMPO_ESCALA = '0.000', TIEMPO_VIDA_TKT, TIEMPO_ESCALA) > 80, 1, 0)) AS 'bajas_80_max',
                 SUM(IF(INTERNALPRIORITY IS NULL, 1, 0)) AS 'nulos',
                 COUNT(TICKETID) AS total_incidentes
             FROM maximo.INCIDENT
